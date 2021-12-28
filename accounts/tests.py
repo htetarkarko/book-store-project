58b4945e8f1c50fd52cc7e15bfc13b092bcc247a
htetarkarko <spotifyhakk88@gmail.com>
2021-12-28 14:22:47 +0630

    configure accounts model and form

---
+++ accounts/tests.py
@@ -0,0 +1,30 @@
+from django.contrib.auth import get_user_model
+from django.test import TestCase
+
+
+class CustomUserTest(TestCase):
+    def test_create_user(self):
+        User = get_user_model()
+        user = User.objects.create_user(
+            username='will',
+            email='will@email.com',
+            password='test123',
+        )
+        self.assertEqual(user.username, 'will')
+        self.assertEqual(user.email, 'will@email.com')
+        self.assertTrue(user.is_active)
+        self.assertFalse(user.is_staff)
+        self.assertFalse(user.is_superuser)
+        
+    def test_create_superuser(self):
+        User = get_user_model()
+        admin_user = User.objects.create_superuser(
+            username='admin',
+            email='admin@email.com',
+            password='test123'
+        )
+        self.assertEqual(admin_user.username, 'admin')
+        self.assertEqual(admin_user.email, 'admin@email.com')
+        self.assertTrue(admin_user.is_active)
+        self.assertTrue(admin_user.is_staff)
+        self.assertTrue(admin_user.is_superuser)
